c2a28dcceb70960223e181e61e64e5ec7a621688
jakekasan <jake.kasan@gmail.com>
2018-06-16 16:00:07 +0200

    coffee sim now works

--- coffee.go
+++ coffee.go
@@ -1,63 +1,111 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "time"
+)
 
-
-type coffee struct {
-  nameOfCoffee string
-  price int
-  requirements []string
-  fulfilled []resource
-}
-
-type resource struct {
-  nameOfResource string
-  cost int
-}
 
 type order struct {
-  coffees []coffee
+  coffee string
   cost int
 }
 
+type machine struct {
+  name string
+}
 
-func generateOrder(name string) coffee {
-  return coffee{nameOfCoffee: name,price: 10, requirements: []string{"water","ground coffee beans"},fulfilled: []resource{}}
+func (m machine) use() {
+  fmt.Println("Using ",m.name)
+  time.Sleep(time.Second)
 }
 
 
+// make order channel
 
+func makeOrderChannel(n int) chan order {
+  return make(chan order,n)
+}
+
+func makeNewMachine(name string) machine {
+  return machine{name: name}
+}
+
+func makeNewMachineChannel(n int,name string) chan machine {
+  machines := make(chan machine,n)
+  for i := 0; i < n; i++ {
+    newMachine := makeNewMachine(name)
+    machines<-newMachine
+  }
+  return machines
+}
+
+func grindCoffeeBeans(grinders chan machine) string {
+  grinder := <-grinders
+  grinder.use()
+  grinders<-grinder
+  return "groundCoffee"
+}
+
+func makeEspresso(groundCoffee string,pressers chan machine) string {
+  presser := <-pressers
+  presser.use()
+  pressers<-presser
+  return "espresso"
+}
+
+func steamMilk(steamers chan machine) string {
+  steamer := <-steamers
+  steamer.use()
+  steamers<-steamer
+  return "steamedMilk"
+}
+
+func makeLatte(steamedMilk string,espresso string) string {
+  return "latte"
+}
+
+func initBarista(orders chan order, grinders chan machine, pressers chan machine, steamers chan machine) {
+  for ord := range orders {
+    switch {
+    case ord.coffee == "espresso":
+      groundCoffee := grindCoffeeBeans(grinders)
+      espresso := makeEspresso(groundCoffee,pressers)
+      fmt.Println("Finished",espresso)
+    case ord.coffee == "latte":
+      groundCoffee := grindCoffeeBeans(grinders)
+      espresso := makeEspresso(groundCoffee,pressers)
+      steamedMilk := steamMilk(steamers)
+      latte := makeLatte(steamedMilk,espresso)
+      fmt.Println("Finished",latte)
+    }
+  }
+}
 
 
 func main(){
   // main stuff!
 
-  resources := make([]resource,0)
+  orders := makeOrderChannel(10)
 
+  grinders := makeNewMachineChannel(2,"grinder")
+  pressers := makeNewMachineChannel(2,"presser")
+  steamers := makeNewMachineChannel(2,"steamer")
 
-  // make water resources
-  for range [5]int{} {
-    resources = append(resources,resource{nameOfResource: "water", cost: 1})
+  for range [5]int{}{
+    orders<-order{coffee: "latte"}
   }
 
-  // make coffee resources
-  for range [5]int{} {
-    resources = append(resources,resource{nameOfResource: "ground coffee beans", cost: 5})
+  for range [5]int{}{
+    orders<-order{coffee: "espresso"}
   }
 
-  var espresso = coffee{nameOfCoffee: "espresso", price: 10, requirements: []string{"water","ground coffee beans"},fulfilled: []resource{}}
-
-  fmt.Println(espresso.nameOfCoffee)
-
-  // get orders
-
-  pendingOrders := make([])
-
-  for range [5]int{} {
-
-  }
+  go initBarista(orders,grinders,pressers,steamers)
+  go initBarista(orders,grinders,pressers,steamers)
 
 
+
+  time.Sleep(time.Second * 60)
 
 
 }
